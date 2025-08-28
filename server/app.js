a0948177530fe7736d5d92151de86afe4d259c33
saidil <aidilsurya9@gmail.com>
2025-08-28 11:47:30 +0700

    add origin

--- server/app.js
+++ server/app.js
@@ -12,8 +12,15 @@ const PORT = process.env.PORT || 5000;
 // MongoDB connection
 connectMongoDB();
 
+const allowedOrigins = ["http://localhost:3000"];
+
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
